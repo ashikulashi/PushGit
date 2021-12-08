d6f9bbbccbc90ad0bd77ce210a3d20a4b7274a7b
balabv321 <balabv321@gmail.com>
2021-12-08 16:29:35 +0530

    added to Ashik

--- src/org/info/Push.java
+++ src/org/info/Push.java
@@ -1,8 +1,19 @@
 package org.info;
 
 public class Push {
+
+	private void add() {
+
+		System.out.println("Add");
+
+	}
+	private void java() {
+		System.out.println("java");
+
+	}
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("push");
 	}
 
